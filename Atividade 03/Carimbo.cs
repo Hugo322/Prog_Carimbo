fe4f7d9c213f3cbcd6a2879192a658cf1aa30aae
Hugo322 <60580574+Hugo322@users.noreply.github.com>
2020-03-27 22:52:13 -0300

    Add files via upload

---
+++ Atividade 03/Carimbo.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_03
+{
+    class Carimbo:Interacao
+    {
+        private string cor;
+        private string texto;
+        private int carga;
+
+        public Carimbo(string TTexto, string CCor, int CCarga, int ent2)
+        {
+            if ( ent2 == 1)
+            {
+                Usar();
+            }
+
+            if ( ent2 == 2)
+            {
+                Carregar(CCor, CCarga);
+            }
+
+            if ( ent2 == 3)
+            {
+                TrocarCor( CCor);
+            }
+
+            if ( ent2 == 4)
+            {
+                TrocarTexto(TTexto);
+            }
+        }
+
+        private string Cor
+        {
+            get { return cor; }
+            set { cor = value;  }
+        }
+        private string Texto
+        {
+            get { return texto;  }
+            set { texto = value;  }
+        }
+        private int Carga
+        {
+            get { return carga; }
+            set { carga = value; }
+        }
+
+        public void Usar()
+        {
+            if (carga >= 1)
+            {
+                if (!String.IsNullOrEmpty(Texto))
+                {
+                    if (!String.IsNullOrEmpty(Cor))
+                    {
+                        Console.Write("(" + Texto + ") na cor " + Cor);
+                        carga--;
+                    }
+                    else
+                    {
+                        Console.Write("Impossível Carimbar - [Sem Cor] ");
+                    }
+                }
+                else
+                {
+                    Console.Write("Impossível Carimbar - [Sem Texto]");
+                }
+            }
+            else
+            {
+                Console.Write("Impossível Carimbar - [Sem Carga]");
+            }
+        }
+        public void Carregar(string CCor, int CCarga)
+        {
+            TrocarCor(CCor);
+            if ((CCarga < 1 && CCarga > 10) && ((Carga + CCarga) > 10))
+            {
+                Console.WriteLine("Carga atual: [" + Carga + "] - Carga deve estar entre 1 e 10.");
+            }
+            else
+            {
+                this.Carga = CCarga;
+            }
+        }
+        public void TrocarCor(string CCor)
+        {
+            if (CCor == Cor)
+            {
+                Console.Write("Carimbo já se encontra com essa cor.");
+            }
+            else
+            {
+               this.Cor = CCor;
+            }
+        }
+        public void TrocarTexto(string TTexto)
+        {
+            if (TTexto == Texto)
+            {
+                Console.Write("Carimbo já se encontra com esse texto.");
+            }
+            else
+            {
+                this.Texto = TTexto;
+            }
+        }
+    }
+}
---
+++ Atividade 03/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ent1;
+            
+            do
+            {
+                Console.WriteLine("----- SELECIONE A FUNÇÃO DESEJADA -----");
+                Console.WriteLine("              1 - Carimbar             ");
+                Console.WriteLine("              2 - Escrever             ");
+                Console.WriteLine("              3 - Sair                 ");
+
+
+                ent1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+
+               if ( ent1 != 3)
+                {
+                    Console.WriteLine("----- SELECIONE A ACAO DESEJADA -----");
+                    Console.WriteLine("           1 - Usar                  ");
+                    Console.WriteLine("           2 - Carregar              ");
+                    Console.WriteLine("           3 - Trocar cor            ");
+                    Console.WriteLine("           4 - Trocar texto          ");
+
+
+                    int ent2 = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+
+                    string CCor = "Preto";
+                    string TTexto = "Boa noite !";
+                    int CCarga = 4;
+
+                    switch (ent2)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter para carimbar.... ");
+                            Console.ReadKey();
+                            Console.Clear();
+
+                            break;
+
+                        case 2:
+
+                            Console.WriteLine("Digite qual a quantidade de carga que deseja adicionar: ");
+                            CCarga = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter para adicionar a carga.... ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+
+                        case 3:
+
+                            Console.WriteLine("Digite a cor desejada: ");
+                            CCor = System.Console.ReadLine();
+                            Console.WriteLine("Enter para trocar a cor.... ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+
+
+                        case 4:
+
+                            Console.WriteLine("Digite o texto desejado: ");
+                            TTexto = System.Console.ReadLine();
+                            Console.WriteLine("Enter para trocar o texto.... ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+
+                    }
+
+                    if (ent2 == 1)
+                    {
+                        Carimbo car = new Carimbo(TTexto, CCor, CCarga, ent2);
+                    }
+
+                    else
+                    {
+                        Caneta can = new Caneta(TTexto, CCor, CCarga, ent2);
+                    }
+                }
+                   
+                                                                         
+           
+            } while (ent1 != 3);
+            
+
+        }
+    }
+}
